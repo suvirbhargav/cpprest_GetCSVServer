05e07275c7e5765d539d3fc42dd1e785f44f32fe
suvir <suvir.bhargav@skf.com>
2020-05-26 10:22:08 +0200

    fixed csv header

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cpprest_GetCSVServer
  cpprest based server serving get request and returns csv file in response.
+
+
+sudo apt install libssl-dev
+sudo apt-get install libcpprest-dev
--- src/handler.cpp
+++ src/handler.cpp
@@ -38,7 +38,24 @@ void handler::handle_get(http_request message) {
 
     concurrency::streams::fstream::open_istream(U("GetFile/Pist 10s again 2_Z-Weighting.csv"), std::ios::in).then(
             [=](concurrency::streams::istream is) {
-                message.reply(status_codes::OK, is, U("text/csv"))
+                message.reply(status_codes::OK, is, U("Connection: keep-alive\n"
+                                                      "Content-Length: 581\n"
+                                                      "Cache-Control: max-age=300\n"
+                                                      "Content-Security-Policy: default-src 'none'; style-src 'unsafe-inline'; sandbox\n"
+                                                      "Content-Type: text/plain; charset=utf-8\n"
+                                                      "Strict-Transport-Security: max-age=31536000\n"
+                                                      "X-Content-Type-Options: nosniff\n"
+                                                      "X-Frame-Options: deny\n"
+                                                      "X-XSS-Protection: 1; mode=block\n"
+                                                      "Via: 1.1 varnish (Varnish/6.0)\n"
+                                                      "Accept-Ranges: bytes\n"
+                                                      "Via: 1.1 varnish\n"
+                                                      "X-Served-By: cache-bma1643-BMA\n"
+                                                      "X-Cache: MISS, HIT\n"
+                                                      "X-Cache-Hits: 0, 1\n"
+                                                      "Vary: Authorization,Accept-Encoding\n"
+                                                      "Access-Control-Allow-Origin: *\n"
+                                                      "Source-Age: 13"))
                         .then([](pplx::task<void> t) {
                             try {
                                 t.get();
