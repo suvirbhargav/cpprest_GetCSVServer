d9b958d476e36a718a0bd38562f2d3649aaf2db4
suvir <suvir.bhargav@skf.com>
2020-06-03 11:16:38 +0200

    cleaned headers

--- src/handler.cpp
+++ src/handler.cpp
@@ -36,44 +36,25 @@ void handler::handle_get(http_request message) {
 
     message.relative_uri().path();
 
-    //set response instance
-    /*http_response response(status_codes::OK);
-    response.headers().set_content_type(U("application/text"));
-    response.headers().set_content_length(3495423);
-    response.headers().set_cache_control(U("max-age=300"));
-    response.headers().add(U("Content-Security-Policy"), U("default-src 'none'; style-src 'unsafe-inline'; sandbox"));
-    response.headers().add(U("Strict-Transport-Security"), U("max-age=31536000"));
-    response.headers().add(U("Accept-Ranges"), U("bytes"));
-    response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
-    response.headers().add(U("Source-Age"), U("13"));*/
+    string_t file_name = "sample.csv";
 
-
-    concurrency::streams::fstream::open_istream(U("GetFile/Pist 10s again 2_Z-Weighting.csv"), std::ios::in).then(
-    //concurrency::streams::fstream::open_istream(U("GetFile/less.csv"), std::ios::in).then(
+    concurrency::streams::fstream::open_istream(U("spectrum/sample.csv"), std::ios::in).then(
             [=](concurrency::streams::istream is) {
-                message.reply(status_codes::OK, is,  U("Content-Type: application/text\n"
-                                                       "Content-Length: 581\n"
-                                                       "Cache-Control: max-age=300\n"
-                                                       "Content-Security-Policy: default-src 'none'; style-src 'unsafe-inline'; sandbox\n"
-                                                       "Strict-Transport-Security: max-age=31536000\n"
-                                                       "X-Content-Type-Options: nosniff\n"
-                                                       "X-Frame-Options: deny\n"
-                                                       "X-XSS-Protection: 1; mode=block\n"
-                                                       "Via: 1.1 varnish (Varnish/6.0)\n"
-                                                       "Accept-Ranges: bytes\n"
-                                                       "Via: 1.1 varnish\n"
-                                                       "X-Served-By: cache-bma1643-BMA\n"
-                                                       "X-Cache: MISS, HIT\n"
-                                                       "X-Cache-Hits: 0, 1\n"
-                                                       "Vary: Authorization,Accept-Encoding\n"
-                                                       "Access-Control-Allow-Origin: *\n"
-                                                       "Source-Age: 13"))
+                web::http::http_response response(web::http::status_codes::OK);
+                response.headers().set_content_type(U("application/text"));
+                response.headers().add(U("Content-Disposition"),U("inline; filename = \"") + file_name + U("\""));
+                response.headers().set_content_length(3495423);
+                response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
+                response.set_body(std::move(is), U("application/text"));
+
+                message.reply(response)
                         .then([](pplx::task<void> t) {
                             try {
                                 t.get();
                             }
                             catch (...) {
                                 //
+                                std::cout << "catch" << std::endl;
                             }
                         });
             }).then([=](pplx::task<void> t) {
@@ -82,6 +63,7 @@ void handler::handle_get(http_request message) {
         }
         catch (...) {
             message.reply(status_codes::InternalError, U("INTERNAL ERROR "));
+            std::cout << "internal error" << std::endl;
         }
     });
 
